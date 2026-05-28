daaa2bbe2a9395612cf8684301c6bea280daccc1
Zerrrook <1221manuelluiz0514@gmail.com>
2026-05-28 20:37:57 +0800

    Forgot this too woops

---
+++ scripts/combat/ConfirmAttackHandler.cs
@@ -0,0 +1,58 @@
+using Godot;
+using NeuralZeroProtocol.Scripts.Cards;
+using NeuralZeroProtocol.Scripts.Characters;
+using NeuralZeroProtocol.Scripts.Resources.CharacterData;
+using NeuralZeroProtocol.Scripts.Resources.MoveData;
+
+namespace NeuralZeroProtocol.Scripts.Combat;
+
+public partial class ConfirmAttackHandler : Node
+{
+	private Card _selectedCard;
+
+	public void SelectedCardData(Character  playerCharacter, EnemyCharacter chosenEnemy)
+	{
+		int damageDealt;
+
+		GD.Print($"Card Power: {_selectedCard.MoveData.Power}");
+		GD.Print($"Card Cost: {_selectedCard.MoveData.Cost}");
+		
+		if (_selectedCard.MoveData.ActionType == ActionType.SpellAction)
+		{
+			// Placeholder, they don't exactly do damage
+			
+			GD.Print($"{playerCharacter} inflicted {_selectedCard.MoveData.Name} to {chosenEnemy}!");
+			return;
+		}
+		
+		(bool isDodged, bool isEnemyImmune, bool isCrit, damageDealt) = DamageCalculator.CalculateDamage(playerCharacter, chosenEnemy, _selectedCard.MoveData);
+
+		if (isDodged)
+		{
+			GD.Print($"{chosenEnemy.Name} has dodged the attack!");
+			return;
+		}
+		
+		if (isEnemyImmune)
+		{
+			GD.Print($"{chosenEnemy.Name} was immune to the attack!");
+			return;
+		}
+		
+		chosenEnemy.HealthComponent.TakeDamage(damageDealt);
+		GD.Print($"{playerCharacter.Name} dealt {damageDealt} to {chosenEnemy.Name}!");
+		
+		if (isCrit)
+		{
+			GD.Print("It was a critical Hit!");
+		}
+	}
+	
+	
+	public void SetCurrentSelectedCard(Card selectedCard)
+	{
+		_selectedCard = null;
+		
+		_selectedCard = selectedCard;
+	}
+}
