a7453f18a05f425e6e15d133f82efb0cbff546f7
Zerrrook <1221manuelluiz0514@gmail.com>
2026-07-08 17:40:23 +0800

    test: Adding Unit Testing For: CharacterStatResource.cs

---
+++ test/scripts/resources/character_data/TestCharacterStatResource.cs
@@ -0,0 +1,31 @@
+﻿using GdUnit4;
+using NeuralZeroProtocol.Autoloads;
+using NeuralZeroProtocol.Scripts.Resources.CharacterData;
+using static GdUnit4.Assertions;
+namespace NeuralZeroProtocol.Tests.Scripts.Resources.CharacterData;
+
+[TestSuite]
+[RequireGodotRuntime]
+public class TestCharacterStatResource
+{
+    private CharacterStatResource _characterStatResource;
+
+    [BeforeTest]
+    public void Setup()
+    {
+        _characterStatResource = AutoFree(new CharacterStatResource());
+    }
+
+    [TestCase(Rarity.Scrap, 1.0f, TestName = "GetStatMultiplier_ReturnsScrapMultiplier")]
+    [TestCase(Rarity.Common, 1.15f, TestName = "GetStatMultiplier_ReturnsCommonMultiplier")]
+    [TestCase(Rarity.Uncommon, 1.4375f, TestName = "GetStatMultiplier_ReturnsUncommonMultiplier")] 
+    [TestCase(Rarity.Rare, 1.796875f, TestName = "GetStatMultiplier_ReturnsRareMultiplier")] 
+    [TestCase(Rarity.SuperRare, 2.6953125f, TestName = "GetStatMultiplier_ReturnsSuperRareMultiplier")] 
+    [TestCase(Rarity.UltraRare, 4.04296875f, TestName = "GetStatMultiplier_ReturnsUltraRareMultiplier")] 
+    public void GetStatMultiplier_ReturnsCorrectMultiplier(Rarity rarity, float expectedMult)
+    {
+        float mult = _characterStatResource.GetStatMultiplier(rarity);
+
+        AssertThat(mult).IsEqual(expectedMult);
+    }
+}
